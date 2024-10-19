302921838bf4bb6e3ca3ae20466613c27df15da1
bycaroline <eklund.caroline@gmail.com>
2024-10-19 15:17:02 +0200

    first commit

---
+++ src/test/java/com/carolineeklund/architecture/ArchitectureApplicationTests.java
@@ -0,0 +1,13 @@
+package com.carolineeklund.architecture;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArchitectureApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
