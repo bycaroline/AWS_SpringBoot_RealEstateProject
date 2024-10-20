99989724ca8e6aef44bb9bd5b31ba29a9166ea14
bycaroline <eklund.caroline@gmail.com>
2024-10-20 18:10:10 +0200

    cors config nytt försök

--- src/main/java/com/carolineeklund/architecture/controllers/HouseController.java
+++ src/main/java/com/carolineeklund/architecture/controllers/HouseController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 import java.util.Optional;
 
-@CrossOrigin(origins = {"http://localhost:*", "http://127.0.0.1:*"})
+@CrossOrigin(origins = "*") 
 @RestController
 @RequestMapping("/house")
 @RequiredArgsConstructor //så att autowired house repo funkar
